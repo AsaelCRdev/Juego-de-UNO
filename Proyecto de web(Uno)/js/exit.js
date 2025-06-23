1b016128a49be4da34d907dd3e62115be37ddbde
AsaelCRdev <166563204+AsaelCRdev@users.noreply.github.com>
2025-06-23 19:27:30 -0400

    Add files via upload

--- Proyecto de web(Uno)/js/exit.js
+++ Proyecto de web(Uno)/js/exit.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const exitButton = document.getElementById('exit')
     if (exitButton) {
         exitButton.addEventListener('click', function() {
-            window.location.href = "index.html";
+            window.location.href = "menu.html";
         });
     } 
 });
