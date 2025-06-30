dd8bcd8b164490d11946a6655a80cf3c09d501ef
SimonM177 <simon.manjoud@gmail.com>
2025-06-29 21:37:56 -0400

    Se agregó todas las funcionalidades para la primera entrega: Bots, cartas jugables, HTML y CSS del tablero, entre otros. Nombres incluídos.

---
+++ Proyecto de web(Uno)/Js/utils/dataLocal.js
@@ -0,0 +1,14 @@
+//Cargar el nombre del jugador y el número de bots utilizando localStorage
+document.addEventListener('DOMContentLoaded', function() {
+    const username = localStorage.getItem('username') || 'Usuario';
+    const numBots = parseInt(localStorage.getItem('numBots')) || 3
+    //Actualizar el nombre del jugador 1
+    document.getElementById('player-1-name').textContent = username
+    //Ocultar bots extras
+    if (numBots < 3) {
+        document.getElementById('player-4').style.display = 'none';
+    }
+    if (numBots < 2) {
+        document.getElementById('player-3').style.display = 'none';
+    }
+});
